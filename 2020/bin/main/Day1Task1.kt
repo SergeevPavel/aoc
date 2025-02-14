6421532c4f1ba752c690b394684a9303bc4e4ee9
pavel.sergeev <sergeev.pavel.al@gmail.com>
2025-02-14 19:20:19 +0100

    123

---
+++ 2020/bin/main/Day1Task1.kt
@@ -0,0 +1,17 @@
+
+// identation in lambdas
+// chaotic backend
+fun main() {
+    val input = object {}.javaClass.getResource("day1.txt").readText()
+    val numbers = input.lines().map { number ->
+        number.toInt()
+    }
+    for (a in numbers) {
+        for (b in numbers) {
+            if (a + b == 2020) {
+                println("a = $a b = $b result = ${a * b}")
+            }
+
+        }
+    }
+}
